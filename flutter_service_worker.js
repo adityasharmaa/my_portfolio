283534f3890685f100a7198245ef06fd6fb3d43e
aditya200202 <aditya.sharma@w3dev.email>
2020-03-28 20:03:58 +0530

    Built web

---
+++ flutter_service_worker.js
@@ -0,0 +1,66 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "26592af158c50cf3b90fac856c065632",
+"assets/assets/about_background.jpg": "aae4e5da03f3323245572fef992f6691",
+"assets/assets/android_image.png": "eae67db22238dca4987094f5feca966d",
+"assets/assets/avatar.jpg": "f293ca2b2b0b3cfab861611c980f1926",
+"assets/assets/background.jpg": "ecda6493e367eaf74b522cf6b00e157f",
+"assets/assets/cpp_image.png": "f01d9a4a208dd0edbcd98f476d8419ed",
+"assets/assets/c_programming_image.png": "1c1888a3eb2f04360763c1a95257b726",
+"assets/assets/facebook.png": "021ada146ffb7c1753557ff29618d04c",
+"assets/assets/fcm_image.png": "af5e2d26a5e6a489041c6c04f3e50a70",
+"assets/assets/firebase_image.png": "e642c7556d83be574ae3929c34ead074",
+"assets/assets/flutter_image.png": "d21f1eecaeaab081ba7efec1721c0712",
+"assets/assets/fonts/GoogleSansRegular.ttf": "000be6a5acfcd4e4a425e9f1bb6c80f0",
+"assets/assets/github.png": "d22ee3727a7216019c3848df6eafa024",
+"assets/assets/git_image.png": "042664999475fd8cc3672db6567e2e53",
+"assets/assets/instagram.png": "26631a4043b14dff84180bdf51c3cacb",
+"assets/assets/java_image.png": "1faf0791eafd8d9fdbe24cabfec303ca",
+"assets/assets/linkedin.png": "926e2dcf5ab4220a359867614556df68",
+"assets/assets/moon.png": "a270b8a10d1a9a52441bf5a322dd1b86",
+"assets/assets/projects/events_info.png": "58074cdb0900d1c82f09ddf056756433",
+"assets/assets/projects/linograph.png": "c600dd3e3217f07646fabc14746e6816",
+"assets/assets/projects/meals_recipe.png": "774ec340dfedd625189ac988241b9c15",
+"assets/assets/projects/my_brand.png": "22b2039d3c364b1d2d9ca125dbb469b3",
+"assets/assets/projects/my_shop.png": "ce46586c594e96d440d699a715134f11",
+"assets/assets/projects/sudoku.png": "8eb32d003d4001b0ee107b6b9e10005f",
+"assets/assets/projects/suvidha_supermarket.png": "3912ae8a2e18cb322f58ddb3acc9cc1c",
+"assets/assets/projects/suvidha_wholesale.png": "9487ca5a8ab7affa8e0338397b9e144e",
+"assets/assets/projects_background.jpg": "0f0fd9ed02dd7c30777a1f4bc3436c4d",
+"assets/assets/skills_background.jpg": "ecda6493e367eaf74b522cf6b00e157f",
+"assets/FontManifest.json": "bb05d491d63534b264dea8213a4a50be",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "954706ef951b9553ecb95d1a66a0f589",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "c9f432f2613f66fd7f418e28aa012cfb",
+"main.dart.js": "1be4ac5251ae0aac8d09e8b647ed1a6c",
+"manifest.json": "cef785009907754132610e83a04682d3"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
